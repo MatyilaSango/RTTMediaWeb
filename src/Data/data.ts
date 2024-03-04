0456218eab3dab7b10bf89303411c3ac218ca5b0
Sango <60422984+MatyilaSango@users.noreply.github.com>
2024-03-04 17:08:53 +0200

    chore: adding data and images for TumMedia

--- src/Data/data.ts
+++ src/Data/data.ts
@@ -1,6 +1,12 @@
 import rdPreview from "../icons/ReddderDownloader-preview.png"
 import twPreview from "../icons/TwiMedia-preview.png"
 import tmPreview from "../icons/TumMedia-preview.png"
+import TmLoginStep1 from "../icons/TmLogin-step1.jpg"
+import TmLoginStep2 from "../icons/TmLogin-step2.jpg"
+import TmLoginStep3 from "../icons/TmLogin-step3.jpg"
+import TmDAStep1 from "../icons/TmDA-step1.png"
+import TmDAStep2 from "../icons/TmDA-step2.png"
+import TmDAStep3 from "../icons/TmDA-step3.png"
 
 export const products = [
     {
@@ -23,3 +29,131 @@ export const products = [
     }
 ]
 
+export const HowToData = {
+    ReddderDownloader: [
+        {
+            title: "How to Log In?",
+            steps: [
+                {
+                    previewShot: "",
+                    heading: "Step 1: Enter login details.",
+                    body: "Enter your login details. Same as your RTTMedia login details."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 2: Click Sign in",
+                    body: "Click the login button or just click the enter button."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 3: Wait",
+                    body: "Wait to be directed to the main page."
+                }
+            ]
+        },
+        {
+            title: "How to Download Account Media?",
+            steps: [
+                {
+                    previewShot: "",
+                    heading: "Step 1: Add a username/subreddit name.",
+                    body: "Copy the user's username/subreddit name in the Reddit url."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 2: Account search",
+                    body: "Paster the account name and search for it. Then select the account name on the dropdown list to start downloading."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 3: Wait. Account is downloading",
+                    body: "When the download process begins, the account will show with all the media being downloaded."
+                }
+            ]
+        }
+    ],
+    TwiMedia: [
+        {
+            title: "How to Log In?",
+            steps: [
+                {
+                    previewShot: "",
+                    heading: "Step 1: Enter login details.",
+                    body: "Enter your login details. Same as your RTTMedia login details."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 2: Click Sign in",
+                    body: "Click the login button or just click the enter button."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 3: Wait",
+                    body: "Wait to be directed to the main page."
+                }
+            ]
+        },
+        {
+            title: "How to Download Account Media?",
+            steps: [
+                {
+                    previewShot: "",
+                    heading: "Step 1: Add a username name.",
+                    body: "Copy the user's username in the Twitter url."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 2: Account search",
+                    body: "Paster the account name and search for it. Then select the account name on the dropdown list to start downloading."
+                },
+                {
+                    previewShot: "",
+                    heading: "Step 3: Wait. Account is downloading",
+                    body: "When the download process begins, the account will show with all the media being downloaded."
+                }
+            ]
+        }
+    ],
+    TumMedia: [
+        {
+            title: "How to Log In?",
+            steps: [
+                {
+                    previewShot: TmLoginStep1,
+                    heading: "Step 1: Enter login details.",
+                    body: "Enter your login details. Same as your RTTMedia login details."
+                },
+                {
+                    previewShot: TmLoginStep2,
+                    heading: "Step 2: Click Sign in",
+                    body: "Click the login button or just click the enter button."
+                },
+                {
+                    previewShot: TmLoginStep3,
+                    heading: "Step 3: Wait",
+                    body: "Wait to be directed to the main page."
+                }
+            ]
+        },
+        {
+            title: "How to Download Account Media?",
+            steps: [
+                {
+                    previewShot: TmDAStep1,
+                    heading: "Step 1: Add a blog name.",
+                    body: "Copy the user's blog name in the Tumblr url."
+                },
+                {
+                    previewShot: TmDAStep2,
+                    heading: "Step 2: Account search",
+                    body: "Paster the blog name and search for it. Then select the account name on the dropdown list to start downloading."
+                },
+                {
+                    previewShot: TmDAStep3,
+                    heading: "Step 3: Wait. Media is downloading",
+                    body: "When the download process begins the account will show with all the media being downloaded."
+                }
+            ]
+        }
+    ]
+}
